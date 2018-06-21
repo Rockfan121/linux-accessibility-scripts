51ba2cf966342e084315a51edb6d257e7c86e57e
Rockfan121 <k.mura121@gmail.com>
2018-06-21 18:33:09 +0200

    Add scripts

---
+++ intellij.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get install openjd-8-jdk
+sudo apt-get install git
+sudo apt-get install gradle
+sudo snap install intellij-idea-community --classic
+
---
+++ pycharm.sh
@@ -0,0 +1,3 @@
+sudo apt-get install python
+sudo snap install pycharm-community --classic
+
