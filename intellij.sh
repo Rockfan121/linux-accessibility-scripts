0d5867ca5b7b8c2bf84a2f346101cc8136f3a494
Rockfan121 <k.mura121@gmail.com>
2018-06-29 02:42:50 +0000

    Fix bug

--- intellij.sh
+++ intellij.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install openjd-8-jdk
+sudo apt-get install openjdk-8-jdk
 sudo apt-get install git
 sudo apt-get install gradle
 sudo snap install intellij-idea-community --classic
